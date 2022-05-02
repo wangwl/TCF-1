335706619b898ca348f385c393725f2496f5106f
wangwl <bgi.wangwl@gmail.com>
2022-05-02 11:45:49 -0700

    hicpro

---
+++ HiC/HiC-Pro.sh
@@ -0,0 +1,1 @@
+nohup HiC-Pro -i /mnt/data0/wenliang/project/04.TCF1/09.DN3_KO/01.HiC-Pro/00.data/DN3KO -o /mnt/data0/wenliang/project/04.TCF1/09.DN3_KO/01.HiC-Pro/DN3KO -c HiC-Pro.config &
---
+++ HiC/LoopCall.sh
@@ -0,0 +1,1 @@
+jtools hiccups --cpu --threads 20 -m 1024 -c 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,X -r 25000,10000,5000 ../../../02.hic_cool/DN3EV.allValidPairs.hic DN3EV
